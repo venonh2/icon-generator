e7061c33f7c322492bbc1dbf699ee2551bd6b7bf
Brendow Evangelista <brendow-evan@hotmail.com>
2023-05-23 20:05:48 -0300

    chore: field credits control

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -1,11 +1,38 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+// https://slashimagine.pro/docs
 
+/**
+ * protected procedure apenas para usuarios logados.
+ */
 export const generateRouter = createTRPCRouter({
-  generateIcon: publicProcedure
+  generateIcon: protectedProcedure
     .input(z.object({ prompt: z.string() }))
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
+      //verifica se o usuario tem pontos para gerar imagens ainda.
+
+      const { count } = await ctx.prisma.user.updateMany({
+        where: {
+          id: ctx.session.user.id,
+          credits: {
+            gte: 1,
+          },
+        },
+        data: {
+          credits: {
+            decrement: 1,
+          },
+        },
+      });
+
+      if (count <= 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Sem creditos disponiveis, por favor adquira mais",
+        });
+      }
       return {
         message: "deu boa" + input.prompt,
       };
